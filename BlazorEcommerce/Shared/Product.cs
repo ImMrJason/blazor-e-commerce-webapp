31c83d4c1d2214ef4355a683f76c838f536cc3fc
SpaGettys <jgettys@gmail.com>
2023-08-03 15:52:38 +0900

    ・Install EntityFramework → Install Microsoft.EntityFrameworkCore → Install Microsoft.EntityFrameworkCore.Design → Install Microsoft.EntityFrameworkCore.SqlServer
    ・Add Data Context and set it up
    
    ・Add the first migration (code-first)
    → dotnet ef migrations add CreateInitial | dotnet ef database update

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlazorEcommerce.Shared
 {
     public class Product
@@ -10,6 +12,7 @@ namespace BlazorEcommerce.Shared
 
         public string ImageUrl { get; set; } = string.Empty;
 
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
