90334bcf70904969309adcd13011a6562fa9e9b2
Jason Gettys <jason@digital-knowledge.co.jp>
2023-08-13 01:11:10 +0900

    ・Introduce the ProductType and ProductVariation models, update the data context, seed the data, and add a new migration.
    dotnet ef migrations add ProductVariants
    dotnet ef database update
    
    ・Remove the unnecessary Global.json file.

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace BlazorEcommerce.Shared
 {
     public class Product
@@ -12,12 +10,10 @@ namespace BlazorEcommerce.Shared
 
         public string ImageUrl { get; set; } = string.Empty;
 
-        [Column(TypeName = "decimal(18,2)")]
-        // The decimal(18,2) means that the database will store the value as a decimal with 18 digits and 2 of them will be after the decimal point.
-        public decimal Price { get; set; }
-
         public Category? Category { get; set; }
 
         public int CategoryId { get; set; }
+
+        public List<ProductVariant> ProductVariants { get; set; } = new List<ProductVariant>();
     }
 }
