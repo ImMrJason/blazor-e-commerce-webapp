8e33f6d4b610f092649874fd13988cb32edd3c05
Jason Gettys <jason@digital-knowledge.co.jp>
2023-08-16 19:48:24 +0900

    Add server functionality to search for products and receive search suggestions.

--- BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -35,5 +35,19 @@ namespace BlazorEcommerce.Server.Controllers
             var result = await _productService.GetProductsByCategory(categoryUrl);
             return Ok(result);
         }
+
+        [HttpGet("search/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> SearchProducts(string searchText)
+        {
+            var result = await _productService.SearchProducts(searchText);
+            return Ok(result);
+        }
+
+        [HttpGet("searchsuggestions/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> SearchProductSearchSuggestions(string searchText)
+        {
+            var result = await _productService.GetProductSuggestions(searchText);
+            return Ok(result);
+        }
     }
 }
--- BlazorEcommerce/Server/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/Server/Services/ProductService/IProductService.cs
@@ -7,6 +7,10 @@
         Task<ServiceResponse<Product>> GetProduct(int productId);
 
         Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl);
+
+        Task<ServiceResponse<List<Product>>> SearchProducts(string searchText);
+
+        Task<ServiceResponse<List<string>>> GetProductSuggestions(string searchText);
     }
 
     public class ProductService : IProductService
@@ -59,5 +63,62 @@
 
             return response;
         }
+
+        public async Task<ServiceResponse<List<Product>>> SearchProducts(string searchText)
+        {
+            // find products where the title or description contains the search text
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await FindProductsBySearchText(searchText)
+            };
+
+            return response;
+        }
+
+        // Note that our implementation here is based on the assumption that all product names and descriptions are in a Latin-based language
+        // that uses the Latin alphabet.
+        public async Task<ServiceResponse<List<string>>> GetProductSuggestions(string searchText)
+        {
+            var products = await FindProductsBySearchText(searchText);
+
+            List<string> result = new List<string>();
+
+            foreach (var product in products)
+            {
+                // only add the prduct title, if the title has the search text in it.
+                if (product.Title.ToLower().Contains(searchText, StringComparison.OrdinalIgnoreCase))
+                {
+                    result.Add(product.Title);
+                }
+
+                // if we have a description, create a list of words from the description.
+                // for each word, if the word contains the search text, add it to the result.
+                if (!string.IsNullOrEmpty(product.Description))
+                {
+                    var punctuation = product.Description.ToLower().Where(char.IsPunctuation).Distinct().ToArray();
+                    // Split() means split on whitespace. the default is to split on whitespace and punctuation.
+                    var words = product.Description.ToLower().Split().Select(w => w.Trim(punctuation));
+
+                    foreach (var word in words)
+                    {
+                        if (word.ToLower().Contains(searchText, StringComparison.OrdinalIgnoreCase) && !result.Contains(word))
+                        {
+                            result.Add(word);
+                        }
+                    }
+                }
+            }
+
+            return new ServiceResponse<List<string>> { Data = result };
+        }
+
+        private async Task<List<Product>> FindProductsBySearchText(string searchText)
+        {
+            return await _context.Products
+                            .Where(p => p.Title.ToLower().Contains(searchText) || p.Description.ToLower().Contains(searchText))
+                            .Include(p => p.Variants)
+                            .ToListAsync();
+        }
+
     }
 }
