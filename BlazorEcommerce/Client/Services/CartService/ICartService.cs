1b02f9143b0f86a305bf4d1440fd974f109974ab
SpaGettys <jgettys@gmail.com>
2023-09-26 16:19:50 +0900

    ・Add cart service for getting and adding items to cart (in local storage)
    ・Add button to add current product to cart
    
    ・Update cart component to reflect number of items added
    
    ・Other class name fixes

---
+++ BlazorEcommerce/Client/Services/CartService/ICartService.cs
@@ -0,0 +1,56 @@
+﻿using Blazored.LocalStorage;
+
+namespace BlazorEcommerce.Client.Services.CartService
+{
+    public interface ICartService
+    {
+        event Action OnChange;
+
+        Task AddToCart(CartItem cartItem);
+
+        Task<List<CartItem>> GetCartItems();
+    }
+
+    public class CartService : ICartService
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public CartService(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public event Action OnChange;
+
+        public async Task AddToCart(CartItem cartItem)
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+
+            if (cart == null)
+            {
+                // create cart if it doesn't exist yet
+                cart = new List<CartItem>();
+            }
+
+            cart.Add(cartItem);
+
+            // set the cart in local storage
+            await _localStorage.SetItemAsync("cart", cart);
+
+            OnChange.Invoke();
+        }
+
+        public async Task<List<CartItem>> GetCartItems()
+        {
+            var cart = await _localStorage.GetItemAsync<List<CartItem>>("cart");
+
+            if (cart == null)
+            {
+                // create cart if it doesn't exist yet
+                cart = new List<CartItem>();
+            }
+
+            return cart;
+        }
+    }
+}
