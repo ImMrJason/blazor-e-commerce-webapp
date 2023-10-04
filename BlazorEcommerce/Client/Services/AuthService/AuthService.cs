e3026964bd14a0d9b5925e47f25835a7d7db88ba
Mr. Jason <jgettys@gmail.com>
2023-10-04 17:53:16 +0900

    ・ Allow users to change their passwords (Part 2) └ Add server-side controller to change password. └ Add client-side service and UI to enable users to change their password.

--- BlazorEcommerce/Client/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Client/Services/AuthService/AuthService.cs
@@ -5,6 +5,8 @@
         Task<ServiceResponse<int>> Register(UserRegister request);
         Task<ServiceResponse<string>> Login(UserLogin request);
         Task<bool> IsUserAuthenticated();
+        Task<ServiceResponse<bool>> ChangePassword(UserChangePassword request);
+
     }
 
     public class AuthService : IAuthService
@@ -31,6 +33,12 @@
         {
             var result = await _http.PostAsJsonAsync("api/auth/register", request);
             return await result.Content.ReadFromJsonAsync<ServiceResponse<int>>();
+        }
+
+        public async Task<ServiceResponse<bool>> ChangePassword(UserChangePassword request)
+        {
+            var result = await _http.PostAsJsonAsync("api/auth/change-password", request.Password);
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
         }
     }
 }
